00faef3f2f8efa243acf4e59e4ab30dbc268f400
blackknight <blackknight7.h@gmail.com>
2016-08-24 15:57:27 +0800

    Simple test

--- Demos/SendSimpleEmail/SimulateSendEmailByBowser.py
+++ Demos/SendSimpleEmail/SimulateSendEmailByBowser.py
@@ -1,8 +1,21 @@
+# coding = utf-8
 import mechanize
 
 br = mechanize.Browser()
 response = br.open("http://www.w3school.com.cn/jsref/jsref_test_regexp.asp")
 html = response.read()
 
-print response.code
-print html
+#print response.code
+#print html
+
+response2 = br.follow_link(text_regex="test()")
+#print response2.code
+#print response2.read()
+
+br.select_form(name="tryitform")
+br.form.set_all_readonly(False)
+br.form['code']='123456'
+br.form['bt']=''
+response3 = br.submit()
+content = response3.get_data()
+print content
